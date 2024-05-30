fccce88c6d0bfaeaaf7ee14221000e9e786e5571
wraith <158517728+mertbnebro@users.noreply.github.com>
2024-05-30 23:25:42 +0300

    komutlar.js

---
+++ komutlar.js
@@ -0,0 +1,4 @@
+  "token": "", kullanıcının tokenini girin
+    "prefix": ".", prefix girin
+    "color": "AAAA", embed mesajı için renk seçeneği ellemenize gerek yok
+    "reason": "" denetim kaydında gözükcek sebepler için
